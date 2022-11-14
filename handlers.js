79f4fd2f491400ba5f04617f525b6f1689199ebb
SAK74 <sergijks@gmail.com>
2022-11-14 23:57:10 +0100

    feat(main): add header

--- handlers.js
+++ handlers.js
@@ -1,17 +1,17 @@
 function fetchData() {
-  return fetch('./books.json')
+  return fetch("./books.json")
     .then((resp) => resp.json())
     .then((data) => data)
     .catch((err) => console.error(err));
 }
 
 function showMore() {
-  this.lastChild.style.visibility = 'visible';
+  this.lastChild.style.visibility = "visible";
 }
 
 function handleClose(ev) {
   ev.stopPropagation();
-  this.parentNode.style.visibility = 'hidden';
+  this.parentNode.style.visibility = "hidden";
 }
 
 function handleDragOver(ev) {
@@ -19,95 +19,98 @@ function handleDragOver(ev) {
 }
 function countTotal() {
   let total = 0;
-  const bag = document.getElementById('bag');
-  const booksInBag = bag.getElementsByClassName('book-wrapper');
+  const bag = document.getElementById("bag");
+  const booksInBag = bag.getElementsByClassName("book-wrapper");
   for (const book of booksInBag) {
-    const price = book.querySelector('.book-content').querySelector('.price').innerHTML.slice(8);
-    const pieces = book.querySelector('.pieces').dataset.pieces;
+    const price = book
+      .querySelector(".book-content")
+      .querySelector(".price")
+      .innerHTML.slice(8);
+    const pieces = book.querySelector(".pieces").dataset.pieces;
     total += price * pieces;
   }
-  document.getElementById('total').innerText = total;
+  document.getElementById("total").innerText = total;
   // add animation to shop-cart & show according entry
-  const cart = document.querySelector('.cart');
-  console.log(total);
+  const cart = document.querySelector(".cart");
   if (total) {
-    bag.querySelector('.control').style.display = 'block';
-    bag.querySelector('.empty').style.display = 'none';
-    cart.style.color = 'lightcoral';
+    bag.querySelector(".control").style.display = "block";
+    bag.querySelector(".empty").style.display = "none";
+    cart.style.color = "lightcoral";
   } else {
-    bag.querySelector('.control').style.display = 'none';
-    bag.querySelector('.empty').style.display = 'block';
-    cart.style.color = 'initial';
+    bag.querySelector(".control").style.display = "none";
+    bag.querySelector(".empty").style.display = "block";
+    cart.style.color = "initial";
   }
-  cart.classList.add('animation');
+  cart.classList.add("animation");
 }
 function handleRemove() {
-  document.getElementById('bag').removeChild(this.parentElement);
+  document.getElementById("bag").removeChild(this.parentElement);
   countTotal();
 }
 function addToBag(id) {
-  const bag = document.getElementById('bag');
+  const bag = document.getElementById("bag");
   const arr = Array.from(bag.children);
   let pieces;
   if (arr.some((elem) => elem.dataset.id === id)) {
-    pieces = bag.querySelector(`[data-id="${id}"]`).querySelector('.pieces');
+    pieces = bag.querySelector(`[data-id="${id}"]`).querySelector(".pieces");
     pieces.dataset.pieces++;
   } else {
     const book = document.getElementById(id).cloneNode(true);
     book.draggable = false;
-    book.removeEventListener('dragstart', handleDragStart);
-    const content = book.querySelector('.book-content');
-    const control = content.querySelector('.control');
+    book.removeEventListener("dragstart", handleDragStart);
+    const content = book.querySelector(".book-content");
+    const control = content.querySelector(".control");
     content.removeChild(control);
-    book.removeAttribute('id');
+    book.removeAttribute("id");
     book.dataset.id = id;
-    pieces = document.createElement('div');
-    pieces.classList.add('pieces');
+    pieces = document.createElement("div");
+    pieces.classList.add("pieces");
     pieces.dataset.pieces = 1;
-    pieces.innerText = 'Pieces: ';
+    pieces.innerText = "Pieces: ";
     content.appendChild(pieces);
-    const close = document.createElement('i');
-    close.className = 'fa-solid fa-circle-xmark';
-    close.classList.add('closeX');
-    close.addEventListener('click', handleRemove);
+    const close = document.createElement("i");
+    close.className = "fa-solid fa-circle-xmark";
+    close.classList.add("closeX");
+    close.addEventListener("click", handleRemove);
     book.appendChild(close);
-    bag.insertBefore(book, bag.querySelector('.control'));
+    bag.insertBefore(book, bag.querySelector(".control"));
   }
   countTotal();
 }
 function handleDrop(ev) {
-  addToBag(ev.dataTransfer.getData('text'));
+  addToBag(ev.dataTransfer.getData("text"));
 }
 
 function handleDragStart(ev) {
-  ev.dataTransfer.setData('text', ev.target.id);
-  ev.dataTransfer.effectAllowed = 'copy';
-  const image = ev.target.querySelector('img').cloneNode();
-  image.style.width = '100px';
-  image.style.height = '150px';
-  image.style.transform = 'translateX(-2000px)';
-  image.id = 'temp';
+  ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.effectAllowed = "copy";
+  const image = ev.target.querySelector("img").cloneNode();
+  image.width = "100px";
+  image.height = "150px";
+  image.style.transform = "translateX(-2000px)";
+  image.id = "temp";
   document.body.appendChild(image);
   ev.dataTransfer.setDragImage(image, -10, -10);
 }
 function handleDragEnd() {
-  document.body.removeChild(document.getElementById('temp'));
+  document.body.removeChild(document.getElementById("temp"));
 }
 function handleAdd() {
   addToBag(this.parentNode.parentNode.parentNode.id);
 }
 function handleSubmit() {
-  sessionStorage.setItem('amount', document.getElementById('total'));
-  location.assign('./formPage');
+  sessionStorage.setItem("amount", document.getElementById("total"));
+  location.assign("./form");
 }
 function showBag() {
-  const isVisible = document.getElementById('bag').style.transform === 'scaleY(1)';
-  const arrow = document.querySelector('.top-icons').querySelector('.arrow');
+  const isVisible =
+    document.getElementById("bag").style.transform === "scaleY(1)";
+  const arrow = document.querySelector(".top-icons").querySelector(".arrow");
   if (isVisible) {
-    document.getElementById('bag').style.transform = 'scaleY(0)';
-    arrow.classList.replace('fa-angle-up', 'fa-angle-down');
+    document.getElementById("bag").style.transform = "scaleY(0)";
+    arrow.classList.replace("fa-angle-up", "fa-angle-down");
   } else {
-    document.getElementById('bag').style.transform = 'scaleY(1)';
-    arrow.classList.replace('fa-angle-down', 'fa-angle-up');
+    document.getElementById("bag").style.transform = "scaleY(1)";
+    arrow.classList.replace("fa-angle-down", "fa-angle-up");
   }
 }
--- mainPage.js
+++ mainPage.js
@@ -1,111 +1,111 @@
 async function start() {
   const books = (await fetchData()).map((book, id) => {
-    const img = document.createElement('img');
+    const img = document.createElement("img");
     img.src = book.imageLink;
-    img.className = 'image';
+    img.className = "image";
     img.draggable = false;
-    const author = document.createElement('p');
+    const author = document.createElement("p");
     author.innerText = book.author;
-    const title = document.createElement('h3');
+    const title = document.createElement("h3");
     title.innerText = book.title;
-    const price = document.createElement('p');
+    const price = document.createElement("p");
     price.innerText = `Price: $${book.price}`;
-    price.className = 'price';
+    price.className = "price";
 
     const nodes = [author, title, price];
-    const content = document.createElement('span');
+    const content = document.createElement("span");
     for (const node of nodes) {
       content.appendChild(node);
     }
-    content.classList.add('book-content');
+    content.classList.add("book-content");
 
-    const more = document.createElement('span');
-    more.innerText = 'Show more';
-    more.className = 'more';
-    const descr = document.createElement('div');
+    const more = document.createElement("span");
+    more.innerText = "Show more";
+    more.className = "more";
+    const descr = document.createElement("div");
     descr.appendChild(title.cloneNode(true));
     descr.appendChild(document.createTextNode(book.description));
-    descr_btn = document.createElement('button');
-    descr_btn.innerText = 'Close';
-    descr_btn.addEventListener('click', handleClose);
+    descr_btn = document.createElement("button");
+    descr_btn.innerText = "Close";
+    descr_btn.addEventListener("click", handleClose);
     descr.appendChild(descr_btn);
     more.appendChild(descr);
 
-    const btn = document.createElement('button');
-    btn.innerText = 'Add to bag';
-    btn.addEventListener('click', handleAdd);
-    btn.classList.add('button');
-    more.addEventListener('click', showMore);
+    const btn = document.createElement("button");
+    btn.innerText = "Add to bag";
+    btn.addEventListener("click", handleAdd);
+    btn.classList.add("button");
+    more.addEventListener("click", showMore);
 
-    const control = document.createElement('div');
+    const control = document.createElement("div");
     control.appendChild(more);
     control.appendChild(btn);
-    control.classList.add('control');
+    control.classList.add("control");
     content.appendChild(control);
 
-    const wrapper = document.createElement('div');
-    wrapper.className = 'book-wrapper';
+    const wrapper = document.createElement("div");
+    wrapper.className = "book-wrapper";
     wrapper.appendChild(img);
     wrapper.appendChild(content);
-    wrapper.addEventListener('dragstart', () => {});
+    wrapper.addEventListener("dragstart", () => {});
     wrapper.id = id;
     wrapper.draggable = true;
-    wrapper.addEventListener('dragstart', handleDragStart);
-    wrapper.addEventListener('dragend', handleDragEnd);
+    wrapper.addEventListener("dragstart", handleDragStart);
+    wrapper.addEventListener("dragend", handleDragEnd);
     return wrapper;
   });
 
-  const catalog = document.createElement('section');
+  const catalog = document.createElement("section");
   for (const book of books) {
     catalog.appendChild(book);
   }
-  catalog.className = 'catalog';
+  catalog.className = "catalog";
 
-  const bagContainer = document.createElement('div');
-  bagContainer.id = 'bag';
-  bagContainer.addEventListener('dragover', handleDragOver);
-  bagContainer.addEventListener('drop', handleDrop);
+  const bagContainer = document.createElement("div");
+  bagContainer.id = "bag";
+  bagContainer.addEventListener("dragover", handleDragOver);
+  bagContainer.addEventListener("drop", handleDrop);
 
-  const totalAmount = document.createElement('span');
-  const bag = bagContainer.getElementsByClassName('book-wrapper');
+  const totalAmount = document.createElement("span");
+  const bag = bagContainer.getElementsByClassName("book-wrapper");
 
-  totalAmount.innerText = '0';
-  totalAmount.id = 'total';
-  const controlLine = document.createElement('div');
+  totalAmount.innerText = "0";
+  totalAmount.id = "total";
+  const controlLine = document.createElement("div");
   controlLine.appendChild(totalAmount);
-  const sbmtBtn = document.createElement('button');
-  sbmtBtn.innerText = 'confirm';
-  sbmtBtn.addEventListener('click', handleSubmit);
-  sbmtBtn.classList.add('button');
+  const sbmtBtn = document.createElement("button");
+  sbmtBtn.innerText = "confirm";
+  sbmtBtn.addEventListener("click", handleSubmit);
+  sbmtBtn.classList.add("button");
   controlLine.appendChild(sbmtBtn);
-  controlLine.className = 'control';
+  controlLine.className = "control";
   bagContainer.appendChild(controlLine);
-  const empty = document.createElement('div');
-  empty.innerHTML = 'The bag is empty...';
-  empty.className = 'empty';
+  const empty = document.createElement("div");
+  empty.innerHTML = "The bag is empty...";
+  empty.className = "empty";
   bagContainer.appendChild(empty);
 
-  const bagIcon = document.createElement('div');
-  bagIcon.className = 'top-icons';
-  const cart = document.createElement('i');
-  cart.className = 'fa-regular fa-cart-shopping cart';
-  cart.addEventListener('dragover', handleDragOver);
-  cart.addEventListener('drop', handleDrop);
-  cart.addEventListener('animationend', () => {
-    cart.classList.remove('animation');
+  const bagIcon = document.createElement("div");
+  bagIcon.className = "top-icons";
+  const cart = document.createElement("i");
+  cart.className = "fa-regular fa-cart-shopping cart";
+  cart.addEventListener("dragover", handleDragOver);
+  cart.addEventListener("drop", handleDrop);
+  cart.addEventListener("animationend", () => {
+    cart.classList.remove("animation");
   });
-  cart.addEventListener('click', showBag);
-  const arrow = document.createElement('i');
-  arrow.addEventListener('click', showBag);
-  arrow.className = 'fa-solid fa-angle-down arrow';
+  cart.addEventListener("click", showBag);
+  const arrow = document.createElement("i");
+  arrow.addEventListener("click", showBag);
+  arrow.className = "fa-solid fa-angle-down arrow";
   bagIcon.appendChild(cart);
   bagIcon.appendChild(arrow);
 
-  const section2 = document.createElement('section');
+  const section2 = document.createElement("section");
   section2.appendChild(bagIcon);
   section2.appendChild(bagContainer);
-  const main = document.createElement('main');
+  const main = document.createElement("main");
   main.appendChild(catalog);
   main.appendChild(section2);
-  document.getElementById('root').appendChild(main);
+  document.getElementById("root").appendChild(main);
 }
